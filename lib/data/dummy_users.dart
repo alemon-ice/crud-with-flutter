a0b2946641947f5f08365ea6d43f0015a9c48df4
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-09-16 22:03:09 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,35 @@
+import 'package:crud_flutter/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Rafael',
+    email: 'rafael.dias@aluno.com.br',
+    password: 'rafael_aluno_1234',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2020/05/16/00/32/wallpapers-5175525_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Bianca Castilho',
+    email: 'bianca.castilho@aluno.com.br',
+    password: 'bianca_aluno_1234',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2020/05/16/00/33/wallpapers-5175527_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Diego Fernandes',
+    email: 'diego.fernandes@aluno.com.br',
+    password: 'diego_aluno_1234',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2020/05/16/00/25/wallpapers-5175518_960_720.png',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Maria Paula',
+    email: 'maria.paula@aluno.com.br',
+    password: 'maria_aluno_1234',
+    avatarUrl: '',
+  ),
+};
---
+++ lib/views/user_list.dart
@@ -0,0 +1,23 @@
+import 'package:crud_flutter/components/user_tile.dart';
+import 'package:crud_flutter/data/dummy_users.dart';
+import 'package:flutter/material.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+      ),
+    );
+  }
+}
