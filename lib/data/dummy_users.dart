f156aaf118872789107514f01c3c9ea8d9ac2561
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-09-17 11:23:58 -0300

    Add Provider and PUT Method

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -3,33 +3,33 @@ import 'package:crud_flutter/models/user.dart';
 const DUMMY_USERS = {
   '1': const User(
     id: '1',
-    name: 'Rafael',
-    email: 'rafael.dias@aluno.com.br',
-    password: 'rafael_aluno_1234',
+    name: 'Rafael Dias',
+    email: 'rafael.dias@client.com',
+    password: 'rafael_client_1234',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2020/05/16/00/32/wallpapers-5175525_960_720.png',
   ),
   '2': const User(
     id: '2',
     name: 'Bianca Castilho',
-    email: 'bianca.castilho@aluno.com.br',
-    password: 'bianca_aluno_1234',
+    email: 'bianca.castilho@client.com',
+    password: 'bianca_client_1234',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2020/05/16/00/33/wallpapers-5175527_960_720.png',
   ),
   '3': const User(
     id: '3',
     name: 'Diego Fernandes',
-    email: 'diego.fernandes@aluno.com.br',
-    password: 'diego_aluno_1234',
+    email: 'diego.fernandes@client.com',
+    password: 'diego_client_1234',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2020/05/16/00/25/wallpapers-5175518_960_720.png',
   ),
   '4': const User(
     id: '4',
     name: 'Maria Paula',
-    email: 'maria.paula@aluno.com.br',
-    password: 'maria_aluno_1234',
+    email: 'maria.paula@client.com',
+    password: 'maria_client_1234',
     avatarUrl: '',
   ),
 };
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,22 +1,35 @@
 import 'package:crud_flutter/components/user_tile.dart';
-import 'package:crud_flutter/data/dummy_users.dart';
+import 'package:crud_flutter/models/user.dart';
+import 'package:crud_flutter/provider/users.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final Users users = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              users.put(User(
+                name: 'Leonardo Moura',
+                email: 'leonardo.moura@client.com',
+                password: 'leonardo_client_1234',
+                avatarUrl:
+                    'https://cdn.pixabay.com/photo/2020/05/16/00/32/wallpapers-5175525_960_720.png',
+              ));
+            },
+          ),
         ],
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
       ),
     );
   }
